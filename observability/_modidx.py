9aa5cf8c46f4f626f00c7105fb1270dd122ca6e3
Bhargav Kowshik <bhargav.kowshik@gmail.com>
2024-11-24 19:31:31 +0530

    Notebook to save Atom entries from medRxiv

--- observability/_modidx.py
+++ observability/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://aiforhealth-xyz.github.io',
                 'git_url': 'https://github.com/aiforhealth-xyz/observability',
                 'lib_path': 'observability'},
-  'syms': {'observability.core': {'observability.core.foo': ('core.html#foo', 'observability/core.py')}}}
+  'syms': {'observability.core': {'observability.core.foo': ('core.html#foo', 'observability/core.py')}, 'observability.medrxiv': {}}}
